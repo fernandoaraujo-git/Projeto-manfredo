f4c285566161caa579a6a7e6483796cbe9f9c5d7
Gabriel_Felipe <gabrielfelipinho2006@gmail.com>
2026-09-16 21:44:45 -0400

    conexao com o banco de dados mariaDB

---
+++ db.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'techmanaus_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+pool.getConnection()
+    .then(conn => {
+        console.log('MariaDB em funcionamento! Conexão estabelecida com sucesso.');
+        conn.release();
+    })
+    .catch(err => console.error('🚩 Panic no DB:', err.message));
+
+module.exports = pool;
